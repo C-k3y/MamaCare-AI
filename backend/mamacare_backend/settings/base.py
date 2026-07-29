087f0ddaa94c73ca86703ccfd279a311aedf4bff
AKN-Antony <kamauanthony345@gmail.com>
2026-07-29 15:49:41 +0300

    Modified the base.py

--- backend/mamacare_backend/settings/base.py
+++ backend/mamacare_backend/settings/base.py
@@ -61,6 +61,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mamacare_backend.wsgi.application'
 
+AUTH_USER_MODEL = 'users.User'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
