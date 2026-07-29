302b05bbcb13f67e66463bcb6540bef3f80a10ce
AKN-Antony <kamauanthony345@gmail.com>
2026-07-29 15:50:33 +0300

    Modified the admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['email', 'username', 'role', 'is_staff', 'is_active']
+    list_filter = ['role', 'is_staff', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role Information', {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Role Information', {'fields': ('role', 'email')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
