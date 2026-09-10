6cc4c7a77f64440ed05dff2b6896c56e17372da1
AKN-Antony <kamauanthony345@gmail.com>
2026-09-10 23:24:13 +0300

    Wire a Risk assessment backend to frontend Dashboard riskcard abd healthchart

--- frontend/src/api/aiApi.js
+++ frontend/src/api/aiApi.js
@@ -4,6 +4,7 @@ import { API_ROUTES } from '../constants/apiRoutes';
 export const aiApi = {
     checkSymptoms: (symptoms) => axiosInstance.post(`${API_ROUTES.MESSAGES.BASE}/symptom-check`, { symptoms }),
     generateMealPlan: (preferences) => axiosInstance.post(`${API_ROUTES.NUTRITION.MEAL_PLANS}/generate`, preferences),
+    getRisks: () => axiosInstance.get('/risks/'),
 };
 
 export default aiApi;
