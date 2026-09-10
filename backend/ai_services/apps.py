0ed709df41b096f1939633aa6b976d3db18af99d
AKN-Antony <kamauanthony345@gmail.com>
2026-09-10 16:43:32 +0300

    Add Ml preprocessing pipeline

--- backend/ai_services/apps.py
+++ backend/ai_services/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class AiServicesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'ai_services'
+
+    def ready(self):
+        import ai_services.signals
