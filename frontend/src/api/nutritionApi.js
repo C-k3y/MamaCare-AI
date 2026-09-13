f9b88f6340f31f7fd4a7603c1ae60680d88aaab4
AKN-Antony <kamauanthony345@gmail.com>
2026-09-14 01:25:17 +0300

    Integrate dynamic meal fetching and interactive water logging

--- frontend/src/api/nutritionApi.js
+++ frontend/src/api/nutritionApi.js
@@ -3,8 +3,10 @@ import { API_ROUTES } from '../constants/apiRoutes';
 
 export const nutritionApi = {
     getMealPlans: () => axiosInstance.get(API_ROUTES.NUTRITION.MEAL_PLANS),
+    getNutritionLogs: () => axiosInstance.get(API_ROUTES.NUTRITION.LOG),
     logMeal: (mealData) => axiosInstance.post(API_ROUTES.NUTRITION.LOG, mealData),
-    logWater: (amount) => axiosInstance.post(API_ROUTES.NUTRITION.WATER, { amount }),
+    getWaterLogs: () => axiosInstance.get(API_ROUTES.NUTRITION.WATER),
+    logWater: (amount) => axiosInstance.post(API_ROUTES.NUTRITION.WATER, { amount_ml: amount }),
 };
 
 export default nutritionApi;
